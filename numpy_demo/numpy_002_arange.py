44fe8392ce1a2b993dc912206d2ddaf3f6370d5a
Thousandhack <1391053697@qq.com>
2021-09-25 16:20:10 +0800

    numpy练习

---
+++ numpy_demo/numpy_002_arange.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+import numpy as np
+
+data_001 = np.arange(start=-10, stop=10, step=2, dtype=int)
+
+print(data_001, data_001.dtype)
+# [-10  -8  -6  -4  -2   0   2   4   6   8] int32
+
+
