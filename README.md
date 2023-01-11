e46f7a544b22ebb8d1ce28d946518036ddbfe016
Thousandhack <1391053697@qq.com>
2023-01-11 23:42:12 +0800

    arange的使用

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # data_analysis
 数据分析
+pip install numpy
---
+++ book_one/demo_p32.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# arange的使用
+# numpy.arange(start, end, step, dtype)
+arr = np.arange(10)
+
+print(arr)
+
+# numpy.arange(start, end, step, dtype)
+# 开始的述职， 结束的数值， 步长 类型
+
+arr2 = np.arange(-10, 10, 2, dtype=float)
+
+print(arr2)
+
+# 对于以上的数据生成，只存 整型和浮点型
